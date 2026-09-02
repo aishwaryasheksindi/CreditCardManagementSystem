afae7eef703ee2db749f2ad69f4aa40768d7298c
aishwayasheksindi <aishwaryasheksindi@gmail.com>
2026-09-03 02:38:04 +0530

    Updated Exeption

---
+++ src/main/java/com/crimsonlogic/creditcardmanagementsystem/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.crimsonlogic.creditcardmanagementsystem.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
